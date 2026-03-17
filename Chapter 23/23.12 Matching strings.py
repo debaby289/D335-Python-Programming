8650ea17c7217a59ee272237d13e3be27613fef0
Devine <simsdevine@gmail.com>
2026-03-16 23:57:53 -0500

    26.03.16

--- Chapter 23/23.12 Matching strings.py
+++ Chapter 23/23.12 Matching strings.py
@@ -25,3 +25,20 @@ Ex: If the input is:
 the output is:
     0 characters match
 '''
+word_one = input()
+word_two = input()
+count = 0
+
+if len(word_one) > len(word_two):
+    final_judgement = word_two
+else:
+    final_judgement = word_one
+
+for word in range(len(final_judgement)):
+    if word_one[word] == word_two[word]:
+        count += 1
+
+if count == 1:
+    print(f'{count} character matches')
+else:
+    print(f'{count} characters match')
